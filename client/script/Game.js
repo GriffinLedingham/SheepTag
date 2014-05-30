8ae3711c509c61b60919cbb5e2dcc675c45e777d
GriffinLedingham <gcledingham@gmail.com>
2014-05-30 08:56:02 -0700

    Removed createjs and set up phaser framework

--- client/script/Game.js
+++ client/script/Game.js
@@ -1,42 +1,91 @@
-var canvas;  
-var stage;      
-var loader;
-var level;
-var preload;
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'sheep-tag', { preload: preload, create: create, update: update });
 
-function init() {
+function preload() {
 
-  canvas = document.getElementById("gameCanvas");
-  stage = new createjs.Stage(canvas);
+    game.load.tilemap('level', 'data/Level1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tiles.png');
 
-  //No content loading currently so just call loading finished
-  handleComplete();
-
-  stage.update(); 
 }
 
-function handleComplete(event) {
-  doneLoading();
+var map;
+var layer;
+var cursors;
+var line;
+var tileHits = [];
+var plotting = false;
+
+function create() {
+    line = new Phaser.Line();
+    game.stage.backgroundColor = '#787878';
+    map = game.add.tilemap('level');
+    map.addTilesetImage('tiles', 'tiles');
+    layer = map.createLayer('Tile Layer 1');
+    layer.resizeWorld();
+    layer.debug = true;
+    cursors = game.input.keyboard.createCursorKeys();
+    game.input.onDown.add(clickTile, this);
 }
 
-function doneLoading(event) {
-  restart();
+function update() {
+
+    if (cursors.left.isDown)
+    {
+        game.camera.x -= 8;
+    }
+    else if (cursors.right.isDown)
+    {
+        game.camera.x += 8;
+    }
+
+    if (cursors.up.isDown)
+    {
+        game.camera.y -= 8;
+    }
+    else if (cursors.down.isDown)
+    {
+        game.camera.y += 8;
+    }
+
 }
 
-function watchRestart() {
-  stage.update(); 
+function clickTile(pointer) {
+
+    if (tileHits.length > 0)
+    {
+        for (var i = 0; i < tileHits.length; i++)
+        {
+            tileHits[i].debug = false;
+        }
+
+        layer.dirty = true;
+    }
+
+    line.start.set(pointer.worldX, pointer.worldY);
+
+    plotting = true;
+
+    console.log(raycast(pointer));
+
 }
 
-function restart() {
-  stage.removeAllChildren();
-  stage.clear();
-  level = new Level(stage);           
+function raycast(pointer) {
 
-  if (!createjs.Ticker.hasEventListener("tick")) { 
-    createjs.Ticker.addEventListener("tick", tick);
-  }                                     
-}
+    line.end.set(pointer.worldX, pointer.worldY);
 
-function tick(event) {
-  stage.update(event);
+    tileHits = layer.getRayCastTiles(line, 4, false, false);
+
+    if (tileHits.length > 0)
+    {
+        //  Just so we can visually see the tiles
+        for (var i = 0; i < tileHits.length; i++)
+        {
+            tileHits[i].debug = true;
+        }
+
+        layer.dirty = true;
+    }
+
+    plotting = false;
+
+    return tileHits[0];
 }
